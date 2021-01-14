095efa9affc868b1e66561256d82a1010d18c601
Eric Zhou <ericzhou1994@gmail.com>
2021-01-13 21:27:51 -0800

    Add files via upload

---
+++ I Am Poor App/I Am Poor App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor App
+//
+//  Created by Eric Zhou on 2020-04-04.
+//  Copyright © 2020 Eric Zhou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
